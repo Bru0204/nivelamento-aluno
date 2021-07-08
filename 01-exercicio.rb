db33f2e185996ebb299f74eafe47ebf4aa87d9c3
Bruna Cristina <cristinabruna1@outlook.com.br>
2021-07-08 15:23:19 -0300

    aula

---
+++ 01-exercicio.rb
@@ -0,0 +1,14 @@
+puts "quantas horas há em um ano?"
+puts 365*24
+puts "quantos minutos há em uma década?"
+puts 1*5256000
+puts "qual a sua idade em segundos?"
+puts (365*21*24*60*60)
+puts "quantos chocolates você pretende comer na vida?"
+puts 50*50
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts (365*1232*24*60*60)
+
+
+
+
